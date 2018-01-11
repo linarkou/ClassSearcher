1f2be813ddca647a20cfc0d6a1ceae2ff38e0fab
LinarAbzaltdinov <ner-1995>
2018-01-11 22:09:04 +0300

    Added getsize method

--- src/KLargestArray.java
+++ src/KLargestArray.java
@@ -5,33 +5,36 @@ import java.util.TreeSet;
  * Data structure for easy getting K largest elements
  */
 public class KLargestArray {
-     private int k;
-     private TreeSet<ClassInfo> lastModifiedClasses = new TreeSet<>(); //by last modified date
-     private ArrayList<ClassInfo> oldClasses = new ArrayList<>();
+    private int k;
+    private TreeSet<ClassInfo> lastModifiedClasses = new TreeSet<>(); //by last modified date
+    private ArrayList<ClassInfo> oldClasses = new ArrayList<>();
 
-     public KLargestArray(int k) {
-         if (k > 0) {
-             this.k = k;
-         } else
-             throw new IllegalArgumentException("Argument k must be positive");
-     }
+    public KLargestArray (int k) {
+        if (k > 0) {
+            this.k = k;
+        } else
+            throw new IllegalArgumentException("Argument k must be positive");
+    }
 
-     public void add(ClassInfo classToAdd) {
-         if (lastModifiedClasses.size() < k) {
-             lastModifiedClasses.add(classToAdd);
-         } else {
-             if (classToAdd.compareTo(lastModifiedClasses.first()) > 0) {
-                 oldClasses.add(lastModifiedClasses.pollFirst());
-                 lastModifiedClasses.add(classToAdd);
-             } else {
-                 oldClasses.add(classToAdd);
-             }
-         }
-     }
+    public void add(ClassInfo classToAdd) {
+        if (lastModifiedClasses.size() < k) {
+            lastModifiedClasses.add(classToAdd);
+        } else {
+            if (classToAdd.compareTo(lastModifiedClasses.first()) > 0) {
+                oldClasses.add(lastModifiedClasses.pollFirst());
+                lastModifiedClasses.add(classToAdd);
+            } else {
+                oldClasses.add(classToAdd);
+            }
+        }
+    }
 
-     public TreeSet<ClassInfo> getLastModifiedClasses() {
-         return lastModifiedClasses;
-     }
+    public TreeSet<ClassInfo> getLastModifiedClasses() {
+        return lastModifiedClasses;
+    }
 
+    public int size() {
+        return oldClasses.size()+lastModifiedClasses.size();
+    }
 
 }
