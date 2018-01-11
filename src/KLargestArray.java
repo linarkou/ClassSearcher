8c3c6c15620b855dd1ff8a1e814a98363d06028d
LinarAbzaltdinov <ner-1995>
2018-01-11 21:06:08 +0300

    Created KLargestArray

---
+++ src/KLargestArray.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+public class KLargestArray {
+     private int k;
+     private TreeSet<ClassInfo> lastModifiedClasses = new TreeSet<>(); //by last modified date
+     private ArrayList<ClassInfo> oldClasses = new ArrayList<>();
+
+     public KLargestArray(int k) {
+         if (k > 0) {
+             this.k = k;
+         } else
+             throw new IllegalArgumentException("Argument k must be positive");
+     }
+
+     public void add(ClassInfo classToAdd) {
+         if (lastModifiedClasses.size() < k) {
+             lastModifiedClasses.add(classToAdd);
+         } else {
+             if (classToAdd.compareTo(lastModifiedClasses.first()) > 0) {
+                 oldClasses.add(lastModifiedClasses.pollFirst());
+                 lastModifiedClasses.add(classToAdd);
+             } else {
+                 oldClasses.add(classToAdd);
+             }
+         }
+     }
+
+
+}
